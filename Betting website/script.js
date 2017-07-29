e4eec6efe17b6ebd9ec135af7bcf0c9b8c8891b0
therx1 <30556235+therx1@users.noreply.github.com>
2017-07-29 11:26:45 -0700

    Add files via upload

---
+++ Betting website/script.js
@@ -0,0 +1,10 @@
+var firstplayer= document.getElementById("first player")
+var secondplayer = document.getElementById("second player")
+var whobet = document.getElementById("whobet")
+var moneysum = document.getElementById("moneysum")
+var bettingpay = document.getElementById("bet")
+var money=500
+var domtest = function(){
+moneysumsum.innerHTML="your betmoney is gsdfjhgsdfg"   
+}
+domtest()
